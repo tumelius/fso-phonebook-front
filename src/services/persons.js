c86cd5ec668f9940e5fb3991620ef9c88cd2d9e0
Tuomas Puro <tuomas.puro@esedu.fi>
2022-11-28 11:00:35 +0200

    correct api-baseurl

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = "http://localhost:3001/api/persons"
+const baseUrl = "https://mushy-bass-headscarf.cyclic.app/api/persons"
 
 const getAll = () => {
   const request = axios.get(baseUrl)
